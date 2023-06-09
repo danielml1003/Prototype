4def77ba1c23d01b78bbcca9f2d4e4495ae3b835
danielml1003 <danielbaravik1003@gmail.com>
2023-06-09 23:47:51 +0300

    second commit added layout and index file

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <View>
+        <div>Home</div>
+    </View>
+  )
+}
+
+export default Home;
