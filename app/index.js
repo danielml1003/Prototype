81e1b1c777e542205ae44dacf347cb4e933d5d02
danielml1003 <danielbaravik1003@gmail.com>
2023-06-13 23:03:53 +0300

    updated stuff

--- app/index.js
+++ app/index.js
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import {Stack, useRouter} from 'expo-router';
 
-import React from 'react'
+import { COLORS, icons, images, SIZES } from '../constants';
+import { ScreenHeaderBtn, Welcome } from '../components';
 
 export const Home = () => {
-  return (
-    <View>
-        <Text>Home</Text>
-    </View>
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{
+        flex:1,
+        backgroundColor:COLORS.lightWhite
+        }}>
+            <Stack.Screen
+                options={{
+                    headerStyle:{backgroundColor: COLORS.lightWhite},
+                }}
+            />
+        </SafeAreaView>
   )
 }
 
